d40da42c9db23820f1dc25a09b026d14de42d5ed
LKochan123 <kochanczyk.lukasz@gmail.com>
2024-02-22 16:51:13 +0100

    TournamentApp test clearance

--- src/test/java/com/lkochan/tournamentapp/TournamentAppApplicationTests.java
+++ src/test/java/com/lkochan/tournamentapp/TournamentAppApplicationTests.java
@@ -1,13 +1,8 @@
 package com.lkochan.tournamentapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TournamentAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
